c41eaf8d4a09d3b1a23e8ae3e695a4d0fb8d0093
Gerard Zinn <gerardjoven2020@hotmail.com>
2023-07-02 19:54:19 +0200

    Removed env-filter

--- src/config/local_tracing.rs
+++ src/config/local_tracing.rs
@@ -2,10 +2,7 @@ use tracing::subscriber::set_global_default;
 use tracing_subscriber::FmtSubscriber;
 
 pub fn initialize_tracing_subscriber() {
-    let subscriber = FmtSubscriber::builder()
-        .with_env_filter("intelli-telemetry-f123=info")
-        .without_time()
-        .finish();
+    let subscriber = FmtSubscriber::builder().without_time().finish();
 
     set_global_default(subscriber).unwrap();
 
