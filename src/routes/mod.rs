1781eacbb1436a49e7a8a7c9ff3573b045db3e2d
Gerard Zinn <gerardjoven2020@hotmail.com>
2023-07-09 19:04:28 +0200

    feat(routes): Added Timemout Layers and rateLimitLayers

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -10,11 +10,14 @@ use crate::{
     states::{AuthState, UserState},
 };
 use axum::{
+    error_handling::HandleErrorLayer,
     middleware,
     routing::{get, post, IntoMakeService},
     Router,
 };
-use std::sync::Arc;
+use hyper::StatusCode;
+use std::{sync::Arc, time::Duration};
+use tower::{buffer::BufferLayer, limit::RateLimitLayer, timeout::TimeoutLayer, ServiceBuilder};
 
 pub(crate) async fn service_routes(database: Arc<Database>) -> IntoMakeService<Router> {
     let auth_state = AuthState::new(&database);
@@ -31,13 +34,36 @@ pub(crate) async fn service_routes(database: Arc<Database>) -> IntoMakeService<R
                 auth_handler,
             )),
         )
-        .with_state(auth_state.clone());
+        .with_state(auth_state.clone())
+        .layer(
+            ServiceBuilder::new()
+                .layer(HandleErrorLayer::new(|e| async move {
+                    (
+                        StatusCode::INTERNAL_SERVER_ERROR,
+                        format!("Unhandled internal error: {}", e),
+                    )
+                }))
+                .layer(BufferLayer::new(1024))
+                .layer(RateLimitLayer::new(5, Duration::from_secs(120))),
+        );
 
     let verify_router = Router::new()
         .route("/email", get(verify_email))
-        .with_state(auth_state);
+        .with_state(auth_state)
+        .layer(
+            ServiceBuilder::new()
+                .layer(HandleErrorLayer::new(|e| async move {
+                    (
+                        StatusCode::INTERNAL_SERVER_ERROR,
+                        format!("Unhandled internal error: {}", e),
+                    )
+                }))
+                .layer(BufferLayer::new(1024))
+                .layer(RateLimitLayer::new(3, Duration::from_secs(30))),
+        );
 
     // todo: Delete Championship, Get Championship
+    // todo: Add Round to Championship, Delete Round from Championship, Get Round from Championship and reference it to the corresponding session
     let championships_router = Router::new()
         .route("/", post(create_championship))
         .route_layer(middleware::from_fn_with_state(
@@ -46,12 +72,20 @@ pub(crate) async fn service_routes(database: Arc<Database>) -> IntoMakeService<R
         ))
         .with_state(user_state);
 
-    // todo: Add Round to Championship, Delete Round from Championship, Get Round from Championship and reference it to the corresponding session
-
     Router::new()
         .route("/", get(init))
         .nest("/auth", auth_router)
         .nest("/verify", verify_router)
         .nest("/championships", championships_router)
+        .layer(
+            ServiceBuilder::new()
+                .layer(HandleErrorLayer::new(|e| async move {
+                    (
+                        StatusCode::INTERNAL_SERVER_ERROR,
+                        format!("Unhandled internal error: {}", e),
+                    )
+                }))
+                .layer(TimeoutLayer::new(Duration::from_secs(3))),
+        )
         .into_make_service()
 }
