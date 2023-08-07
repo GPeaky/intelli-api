bd08330e6768bda3096af14ccb6a1e638016748e
Gerard Zinn <gerardjoven2020@hotmail.com>
2023-08-07 20:39:27 +0200

    fix(states): Removed Web Socket State

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -11,7 +11,7 @@ use crate::{
         verify::verify_email,
     },
     middlewares::auth_handler,
-    states::{AuthState, UserState, WebSocketState},
+    states::{AuthState, UserState},
 };
 use axum::{
     error_handling::HandleErrorLayer,
@@ -36,7 +36,6 @@ async fn handle_error(e: Box<dyn std::error::Error + Send + Sync>) -> (StatusCod
 pub(crate) async fn service_routes(database: Arc<Database>) -> IntoMakeService<Router> {
     let auth_state = AuthState::new(&database);
     let user_state = UserState::new(&database).await;
-    // let web_socket_state = WebSocketState::new(&database);
 
     let cors_layer = CorsLayer::new()
         .allow_origin(AllowOrigin::any())
--- src/states/mod.rs
+++ src/states/mod.rs
@@ -1,7 +1,5 @@
 mod auth;
 mod user;
-mod web_socket;
 
 pub(crate) use auth::*;
 pub(crate) use user::*;
-pub(crate) use web_socket::*;
