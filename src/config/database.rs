5bb927bf75c0d3d7ce61dc1452266806c4a87d94
Gerard Zinn <gerardjoven2020@hotmail.com>
2023-08-17 04:25:33 +0200

    feat(prepared statements): Finished refactor

--- src/config/database.rs
+++ src/config/database.rs
@@ -53,7 +53,7 @@ impl Database {
         let mut statements: AHashMap<String, PreparedStatement> = AHashMap::new();
 
         //* User Tasks
-        let insert_user_task = session
+        let user_insert_task = session
             .prepare("INSERT INTO users (id, username, password, email, active, role, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
 
         let user_email_by_email_task =
@@ -64,19 +64,19 @@ impl Database {
         let user_by_email_task =
             session.prepare("SELECT * FROM users where email = ? ALLOW FILTERING");
 
-        let delete_user_task = session.prepare("DELETE FROM users WHERE id = ?");
+        let user_delete_task = session.prepare("DELETE FROM users WHERE id = ?");
 
-        let activate_user_task = session.prepare("UPDATE users SET active = true WHERE id = ?");
+        let user_activate_task = session.prepare("UPDATE users SET active = true WHERE id = ?");
 
-        let deactivate_user_task = session.prepare("UPDATE users SET active = false WHERE id = ?");
+        let user_deactivate_task = session.prepare("UPDATE users SET active = false WHERE id = ?");
 
         //* Championships Tasks
-        let insert_championships_task = session
+        let championship_insert_task = session
             .prepare(
                 "INSERT INTO championships (id, name, port, user_id, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?)",
             );
 
-        let championships_by_user_id_task =
+        let championship_by_user_id_task =
             session.prepare("SELECT * FROM championships where user_id = ? ALLOW FILTERING");
 
         let championship_by_id_task = session.prepare("SELECT * FROM championships where id = ?");
@@ -86,118 +86,118 @@ impl Database {
 
         let championships_ports_task = session.prepare("SELECT port FROM championships");
 
-        let delete_championship_task = session.prepare("DELETE FROM championships WHERE id = ?");
+        let championship_delete_task = session.prepare("DELETE FROM championships WHERE id = ?");
 
         //* Event Data Tasks
-        let select_event_data_task =
+        let event_data_select_task =
             session.prepare("SELECT * FROM event_data WHERE session_id = ? AND string_code = ?;");
 
-        let insert_event_data_task = session
+        let event_data_insert_task = session
             .prepare("INSERT INTO event_data (session_id, string_code, events) VALUES (?,?,?);");
 
-        let update_event_data_task = session.prepare(
+        let event_data_update_task = session.prepare(
             "UPDATE event_data SET events = events + ? WHERE session_id = ? AND string_code = ?;",
         );
 
-        let events_data_task = session.prepare("SELECT * FROM event_data WHERE session_id = ?;");
+        let event_data_info = session.prepare("SELECT * FROM event_data WHERE session_id = ?;");
 
         //* Other Tasks
-        let insert_lap_data_task =
+        let lap_data_insert_task =
             session.prepare("INSERT INTO lap_data (session_id, lap) VALUES (?,?);");
 
-        let insert_game_session_task =
+        let game_session_insert_task =
             session.prepare("INSERT INTO game_sessions (id, data) VALUES (?,?);");
 
-        let insert_participant_data_task = session
+        let participant_data_insert_task = session
             .prepare("INSERT INTO participants_data (session_id, participants) VALUES (?,?);");
 
-        let insert_final_classification_data_task = session.prepare(
+        let final_classification_insert_task = session.prepare(
             "INSERT INTO final_classification_data (session_id, classification) VALUES (?,?);",
         );
 
         let (
-            insert_user,
+            user_insert,
             user_email_by_email,
             user_by_id,
             user_by_email,
-            delete_user,
-            activate_user,
-            deactivate_user,
-            insert_championships,
+            user_delete,
+            user_activate,
+            user_deactivate,
+            championship_insert,
             championship_by_name,
             championships_ports,
             championship_by_id,
-            insert_game_session,
-            insert_lap_data,
-            select_event_data,
-            insert_event_data,
-            update_event_data,
-            insert_participant_data,
-            insert_final_classification_data,
-            events_data,
-            championships_by_user_id,
-            delete_championship,
+            game_session_insert,
+            lap_data_insert,
+            event_data_select,
+            event_data_insert,
+            event_data_update,
+            participant_data_insert,
+            final_classification_insert,
+            event_data_info,
+            championship_by_user_id,
+            championship_delete,
         ) = try_join!(
-            insert_user_task,
+            user_insert_task,
             user_email_by_email_task,
             user_by_id_task,
             user_by_email_task,
-            delete_user_task,
-            activate_user_task,
-            deactivate_user_task,
-            insert_championships_task,
+            user_delete_task,
+            user_activate_task,
+            user_deactivate_task,
+            championship_insert_task,
             championship_by_name_task,
             championships_ports_task,
             championship_by_id_task,
-            insert_game_session_task,
-            insert_lap_data_task,
-            select_event_data_task,
-            insert_event_data_task,
-            update_event_data_task,
-            insert_participant_data_task,
-            insert_final_classification_data_task,
-            events_data_task,
-            championships_by_user_id_task,
-            delete_championship_task
+            game_session_insert_task,
+            lap_data_insert_task,
+            event_data_select_task,
+            event_data_insert_task,
+            event_data_update_task,
+            participant_data_insert_task,
+            final_classification_insert_task,
+            event_data_info,
+            championship_by_user_id_task,
+            championship_delete_task
         )
         .unwrap();
 
         //* User Statements
-        statements.insert("user.insert".to_string(), insert_user);
+        statements.insert("user.insert".to_string(), user_insert);
         statements.insert("user.email_by_email".to_string(), user_email_by_email);
         statements.insert("user.by_id".to_string(), user_by_id);
         statements.insert("user.by_email".to_string(), user_by_email);
-        statements.insert("user.delete".to_string(), delete_user);
-        statements.insert("user.activate".to_string(), activate_user);
-        statements.insert("user.deactivate".to_string(), deactivate_user);
+        statements.insert("user.delete".to_string(), user_delete);
+        statements.insert("user.activate".to_string(), user_activate);
+        statements.insert("user.deactivate".to_string(), user_deactivate);
 
         //* Championship Statements
-        statements.insert("championship.insert".to_owned(), insert_championships);
+        statements.insert("championship.insert".to_owned(), championship_insert);
         statements.insert("championship.ports".to_string(), championships_ports);
         statements.insert("championship.by_id".to_string(), championship_by_id);
-        statements.insert("championship.by_user".to_string(), championships_by_user_id);
-        statements.insert("championship.delete".to_string(), delete_championship);
+        statements.insert("championship.by_user".to_string(), championship_by_user_id);
+        statements.insert("championship.delete".to_string(), championship_delete);
         statements.insert(
             "championship.name_by_name".to_string(),
             championship_by_name,
         );
 
         //* Event Data Statements
-        statements.insert("event_data.get".to_string(), select_event_data);
-        statements.insert("event_data.insert".to_string(), insert_event_data);
-        statements.insert("event_data.update".to_string(), update_event_data);
-        statements.insert("event_data.events_by_id".to_string(), events_data);
+        statements.insert("event_data.get".to_string(), event_data_select);
+        statements.insert("event_data.insert".to_string(), event_data_insert);
+        statements.insert("event_data.update".to_string(), event_data_update);
+        statements.insert("event_data.events_by_id".to_string(), event_data_info);
 
         // TODO: Update this to use the new statements
-        statements.insert("insert_game_session".to_string(), insert_game_session);
-        statements.insert("insert_lap_data".to_string(), insert_lap_data);
+        statements.insert("insert_game_session".to_string(), game_session_insert);
+        statements.insert("insert_lap_data".to_string(), lap_data_insert);
         statements.insert(
             "insert_participant_data".to_string(),
-            insert_participant_data,
+            participant_data_insert,
         );
         statements.insert(
             "insert_final_classification_data".to_string(),
-            insert_final_classification_data,
+            final_classification_insert,
         );
 
         statements
