d9c75c7df48a3e2d512d286975b82355d737d6cc
Gerard Zinn <gerardjoven2020@hotmail.com>
2023-08-21 01:48:59 +0200

    feat(logs): added logs to admin middleware

--- src/middlewares/admin.rs
+++ src/middlewares/admin.rs
@@ -3,6 +3,7 @@ use crate::{
     error::{AppResult, UserError},
 };
 use axum::{http::Request, middleware::Next, response::Response};
+use tracing::info;
 
 pub async fn admin_handler<T>(req: Request<T>, next: Next<T>) -> AppResult<Response> {
     let user = req
@@ -11,6 +12,10 @@ pub async fn admin_handler<T>(req: Request<T>, next: Next<T>) -> AppResult<Respo
         .ok_or(UserError::Unauthorized)?;
 
     if user.role.ne(&Role::Admin) {
+        info!(
+            "User: {}, tried to access to admin area without having admin role",
+            user.id
+        );
         Err(UserError::Unauthorized)?
     }
 
