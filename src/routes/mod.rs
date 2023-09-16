cf13db1f077b24aeded6ab8fb7082dfba7e1cb48
Gerard Zinn <gerardjoven2020@hotmail.com>
2023-09-16 19:11:33 +0200

    feat(cors): Implemented Cors Again

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,8 +1,10 @@
 use crate::config::Database;
+use axum::routing::any;
 use axum::{error_handling::HandleErrorLayer, routing::IntoMakeService, Router};
 use hyper::StatusCode;
 use std::{sync::Arc, time::Duration};
 use tower::{load_shed::LoadShedLayer, ServiceBuilder};
+use tower_http::cors::{AllowOrigin, CorsLayer};
 
 mod api;
 
@@ -16,13 +18,16 @@ pub async fn handle_error(e: Box<dyn std::error::Error + Send + Sync>) -> (Statu
 
 #[inline(always)]
 pub(crate) async fn service_routes(database: Arc<Database>) -> IntoMakeService<Router> {
+    let cors_layer = CorsLayer::new().allow_origin(AllowOrigin::any());
+
     Router::new()
         .nest("/", api::api_router(database).await)
         .layer(
             ServiceBuilder::new()
                 .layer(HandleErrorLayer::new(handle_error))
                 .layer(LoadShedLayer::new())
-                .timeout(Duration::from_secs(10)),
+                .layer(cors_layer)
+                .timeout(Duration::from_secs(4)),
         )
         .into_make_service()
 }
