cb23ff4e0aa6ab3b64149bb531ca2e311c7e6cce
Gerard Zinn <gerardjoven2020@hotmail.com>
2023-09-27 01:35:08 +0200

    feat(proto): Removed all protobuf implementation

--- src/test/mod.rs
+++
@@ -1,8 +0,0 @@
-pub(crate) mod car_motion_data;
-pub(crate) mod event_data;
-pub(crate) mod final_classification;
-pub(crate) mod participants;
-pub(crate) mod session_data;
-pub(crate) mod session_history;
-
-include!(concat!(env!("OUT_DIR"), "/packet_header_capnp.rs"));
