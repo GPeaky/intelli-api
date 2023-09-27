ed3e10f702bcca833322722a0b9cd9240c103826
Gerard Zinn <gerardjoven2020@hotmail.com>
2023-09-27 16:46:46 +0200

    Revert "feat(packet_header): Implemented convert function"
    This reverts commit f4cbe9f4989e5f670e75a5cf481846ab2303e0a6.

--- src/capnp/mod.rs
+++ src/capnp/mod.rs
@@ -1,6 +1,5 @@
 use crate::dtos::F123Data;
 use capnp::serialize;
-use packet_header_capnp::packet_header::PacketType;
 
 pub mod car_motion_capnp;
 pub mod event_data_capnp;
@@ -10,6 +9,16 @@ pub mod participants_capnp;
 pub mod session_data_capnp;
 pub mod session_history_capnp;
 
+#[repr(u8)]
+pub enum PacketId {
+    Motion,
+    EventData,
+    FinalClassification,
+    Participants,
+    Session,
+    SessionHistory,
+}
+
 #[inline(always)]
 pub fn serialize_to_packet(data: F123Data) -> Vec<u8> {
     let mut payload = Vec::new();
@@ -18,37 +27,37 @@ pub fn serialize_to_packet(data: F123Data) -> Vec<u8> {
         F123Data::Motion(motion) => {
             let message = car_motion_capnp::convert(motion);
             serialize::write_message(&mut payload, &message).unwrap();
-            PacketType::CarMotion
+            PacketId::Motion
         }
 
         F123Data::SessionHistory(history_data) => {
             let message = session_history_capnp::convert(history_data);
             serialize::write_message(&mut payload, &message).unwrap();
-            PacketType::SessionHistoryData
+            PacketId::SessionHistory
         }
 
         F123Data::Session(session) => {
             let message = session_data_capnp::convert(session);
             serialize::write_message(&mut payload, &message).unwrap();
-            PacketType::SessionData
+            PacketId::Session
         }
 
         F123Data::Participants(participants) => {
             let message = participants_capnp::convert(participants);
             serialize::write_message(&mut payload, &message).unwrap();
-            PacketType::Participants
+            PacketId::Participants
         }
 
         F123Data::Event(event) => {
             let message = event_data_capnp::convert(event);
             serialize::write_message(&mut payload, &message).unwrap();
-            PacketType::EventData
+            PacketId::EventData
         }
 
         F123Data::FinalClassification(classification) => {
             let message = final_classification_capnp::convert(classification);
             serialize::write_message(&mut payload, &message).unwrap();
-            PacketType::FinalClassificationData
+            PacketId::FinalClassification
         }
     };
 
