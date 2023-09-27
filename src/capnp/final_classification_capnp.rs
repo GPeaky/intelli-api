68800e38c85752c072fe4775079a1b6bd95c9f1f
Gerard Zinn <gerardjoven2020@hotmail.com>
2023-09-27 13:01:20 +0200

    fix(clippy): Fixed clippy errors

--- src/capnp/final_classification_capnp.rs
+++ src/capnp/final_classification_capnp.rs
@@ -14,20 +14,20 @@ pub fn convert(value: Box<PacketFinalClassificationData>) -> Builder<HeapAllocat
     let mut final_classification_data_list = packet_final_classification_data
         .init_classification_data(value.m_classificationData.len() as u32);
 
-    for (i, classificationData) in value.m_classificationData.into_iter().enumerate() {
+    for (i, classification_data) in value.m_classificationData.into_iter().enumerate() {
         let mut final_classification_data = final_classification_data_list.reborrow().get(i as u32);
 
-        final_classification_data.set_position(classificationData.m_position);
-        final_classification_data.set_num_laps(classificationData.m_numLaps);
-        final_classification_data.set_grid_position(classificationData.m_gridPosition);
-        final_classification_data.set_points(classificationData.m_points);
-        final_classification_data.set_num_pit_stops(classificationData.m_numPitStops);
-        final_classification_data.set_result_status(classificationData.m_resultStatus);
-        final_classification_data.set_best_lap_time_in_m_s(classificationData.m_bestLapTimeInMS);
-        final_classification_data.set_total_race_time(classificationData.m_totalRaceTime);
-        final_classification_data.set_penalties_time(classificationData.m_penaltiesTime);
-        final_classification_data.set_num_penalties(classificationData.m_numPenalties);
-        final_classification_data.set_num_tyre_stints(classificationData.m_numTyreStints);
+        final_classification_data.set_position(classification_data.m_position);
+        final_classification_data.set_num_laps(classification_data.m_numLaps);
+        final_classification_data.set_grid_position(classification_data.m_gridPosition);
+        final_classification_data.set_points(classification_data.m_points);
+        final_classification_data.set_num_pit_stops(classification_data.m_numPitStops);
+        final_classification_data.set_result_status(classification_data.m_resultStatus);
+        final_classification_data.set_best_lap_time_in_m_s(classification_data.m_bestLapTimeInMS);
+        final_classification_data.set_total_race_time(classification_data.m_totalRaceTime);
+        final_classification_data.set_penalties_time(classification_data.m_penaltiesTime);
+        final_classification_data.set_num_penalties(classification_data.m_numPenalties);
+        final_classification_data.set_num_tyre_stints(classification_data.m_numTyreStints);
     }
 
     message
