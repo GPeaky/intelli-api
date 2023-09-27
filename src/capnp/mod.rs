f4cbe9f4989e5f670e75a5cf481846ab2303e0a6
Gerard Zinn <gerardjoven2020@hotmail.com>
2023-09-27 13:08:52 +0200

    feat(packet_header): Implemented convert function

--- src/capnp/mod.rs
+++ src/capnp/mod.rs
@@ -1,5 +1,6 @@
 use crate::dtos::F123Data;
 use capnp::serialize;
+use packet_header_capnp::packet_header::PacketType;
 
 pub mod car_motion_capnp;
 pub mod event_data_capnp;
@@ -9,16 +10,6 @@ pub mod participants_capnp;
 pub mod session_data_capnp;
 pub mod session_history_capnp;
 
-#[repr(u8)]
-pub enum PacketId {
-    Motion,
-    EventData,
-    FinalClassification,
-    Participants,
-    Session,
-    SessionHistory,
-}
-
 #[inline(always)]
 pub fn serialize_to_packet(data: F123Data) -> Vec<u8> {
     let mut payload = Vec::new();
@@ -27,37 +18,37 @@ pub fn serialize_to_packet(data: F123Data) -> Vec<u8> {
         F123Data::Motion(motion) => {
             let message = car_motion_capnp::convert(motion);
             serialize::write_message(&mut payload, &message).unwrap();
-            PacketId::Motion
+            PacketType::CarMotion
         }
 
         F123Data::SessionHistory(history_data) => {
             let message = session_history_capnp::convert(history_data);
             serialize::write_message(&mut payload, &message).unwrap();
-            PacketId::SessionHistory
+            PacketType::SessionHistoryData
         }
 
         F123Data::Session(session) => {
             let message = session_data_capnp::convert(session);
             serialize::write_message(&mut payload, &message).unwrap();
-            PacketId::Session
+            PacketType::SessionData
         }
 
         F123Data::Participants(participants) => {
             let message = participants_capnp::convert(participants);
             serialize::write_message(&mut payload, &message).unwrap();
-            PacketId::Participants
+            PacketType::Participants
         }
 
         F123Data::Event(event) => {
             let message = event_data_capnp::convert(event);
             serialize::write_message(&mut payload, &message).unwrap();
-            PacketId::EventData
+            PacketType::EventData
         }
 
         F123Data::FinalClassification(classification) => {
             let message = final_classification_capnp::convert(classification);
             serialize::write_message(&mut payload, &message).unwrap();
-            PacketId::FinalClassification
+            PacketType::FinalClassificationData
         }
     };
 
--- src/capnp/packet_header_capnp.rs
+++ src/capnp/packet_header_capnp.rs
@@ -1,9 +1,15 @@
-use super::PacketId;
 use capnp::message::{Builder, HeapAllocator};
+use packet_header::PacketType;
 
 include!(concat!(env!("OUT_DIR"), "/packet_header_capnp.rs"));
 
 #[inline(always)]
-pub fn convert(packet_type: PacketId, payload: Vec<u8>) -> Builder<HeapAllocator> {
-    todo!()
+pub fn convert(packet_type: PacketType, payload: Vec<u8>) -> Builder<HeapAllocator> {
+    let mut message = Builder::new_default();
+    let mut packet_header = message.init_root::<packet_header::Builder>();
+
+    packet_header.set_packet_type(packet_type);
+    packet_header.set_payload(payload.as_slice());
+
+    message
 }
