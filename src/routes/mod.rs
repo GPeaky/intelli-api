2733bde3cd7a026b399c73aeba7744199effcc24
Gerard Zinn <gerardjoven2020@hotmail.com>
2023-10-14 16:57:30 +0200

    Implementing metrics

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,5 +1,6 @@
 use crate::config::Database;
 use axum::{error_handling::HandleErrorLayer, http::HeaderValue, routing::IntoMakeService, Router};
+use axum_otel_metrics::HttpMetricsLayerBuilder;
 use hyper::{Method, StatusCode};
 use std::{sync::Arc, time::Duration};
 use tower::{load_shed::LoadShedLayer, ServiceBuilder};
@@ -17,6 +18,11 @@ pub async fn handle_error(e: Box<dyn std::error::Error + Send + Sync>) -> (Statu
 
 #[inline(always)]
 pub(crate) async fn service_routes(database: Arc<Database>) -> IntoMakeService<Router> {
+    let metrics = HttpMetricsLayerBuilder::new()
+        .with_service_name("intelli_api".to_owned())
+        .with_service_version("0.1.0".to_owned())
+        .build();
+
     let cors_layer = CorsLayer::new()
         .allow_origin(AllowOrigin::list(vec![
             HeaderValue::from_static("https://intellitelemetry.live"),
@@ -27,11 +33,13 @@ pub(crate) async fn service_routes(database: Arc<Database>) -> IntoMakeService<R
 
     Router::new()
         .nest("/", api::api_router(database).await)
+        .merge(metrics.routes())
         .layer(
             ServiceBuilder::new()
                 .layer(HandleErrorLayer::new(handle_error))
                 .layer(LoadShedLayer::new())
                 .layer(cors_layer)
+                .layer(metrics)
                 .timeout(Duration::from_secs(4)),
         )
         .into_make_service()
