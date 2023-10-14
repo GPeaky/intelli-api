0365e0b83642e00c50e8e837c838611c93eae5eb
Gerard Zinn <gerardjoven2020@hotmail.com>
2023-10-14 19:27:30 +0200

    Trying to fix m_name

--- src/protos/participants.rs
+++ src/protos/participants.rs
@@ -3,9 +3,12 @@ include!(concat!(env!("OUT_DIR"), "/protos.participants.rs"));
 use crate::dtos::{
     PacketParticipantsData as BPacketParticipantsData, ParticipantData as BParticipantData,
 };
+use std::ffi::CStr;
 
 impl From<BParticipantData> for ParticipantData {
     fn from(value: BParticipantData) -> Self {
+        let c_str = CStr::from_bytes_until_nul(&value.m_name).unwrap();
+
         Self {
             m_ai_controlled: value.m_aiControlled as u32,
             m_driver_id: value.m_driverId as u32,
@@ -14,7 +17,7 @@ impl From<BParticipantData> for ParticipantData {
             m_my_team: value.m_myTeam as u32,
             m_race_number: value.m_raceNumber as u32,
             m_nationality: value.m_nationality as u32,
-            m_name: String::from_utf8(value.m_name.to_vec()).unwrap(),
+            m_name: c_str.to_str().unwrap().to_string(),
             m_your_telemetry: value.m_yourTelemetry as u32,
             m_show_online_names: value.m_showOnlineNames as u32,
             m_platform: value.m_platform as u32,
