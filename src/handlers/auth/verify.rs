692927ea29dd6f5913b82ef6d8ecd39dc710d3f7
Gerard Zinn <gerardjoven2020@hotmail.com>
2023-10-30 22:11:21 +0100

    Removed Logs

--- src/handlers/auth/verify.rs
+++ src/handlers/auth/verify.rs
@@ -9,7 +9,6 @@ use axum::{
     response::{IntoResponse, Response},
 };
 use hyper::StatusCode;
-use tracing::info;
 
 #[inline(always)]
 pub async fn verify_email(
@@ -17,11 +16,9 @@ pub async fn verify_email(
     Query(query): Query<VerifyEmailParams>,
 ) -> AppResult<Response> {
     let token_data = state.token_service.validate(&query.token)?;
-    info!("{:?}", token_data.claims);
     if token_data.claims.token_type.ne(&TokenType::Email) {
         Err(TokenError::InvalidToken)?
     }
-    info!("User id: {}", token_data.claims.sub);
 
     state
         .user_service
