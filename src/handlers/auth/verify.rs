a97a1983856f3185ae6cde6897dc559963f5b7db
Gerard Zinn <gerardjoven2020@hotmail.com>
2023-10-30 21:31:44 +0100

    Trying to get error

--- src/handlers/auth/verify.rs
+++ src/handlers/auth/verify.rs
@@ -9,6 +9,7 @@ use axum::{
     response::{IntoResponse, Response},
 };
 use hyper::StatusCode;
+use tracing::info;
 
 #[inline(always)]
 pub async fn verify_email(
@@ -16,9 +17,11 @@ pub async fn verify_email(
     Query(query): Query<VerifyEmailParams>,
 ) -> AppResult<Response> {
     let token_data = state.token_service.validate(&query.token)?;
+    info!("{:?}", token_data.claims);
     if token_data.claims.token_type.ne(&TokenType::Email) {
         Err(TokenError::InvalidToken)?
     }
+    info!("User id: {}", token_data.claims.sub);
 
     state
         .user_service
