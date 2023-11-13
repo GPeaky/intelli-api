79f5f2ebc501c75e6da0edb5912a1a41d2cfc845
GPeaky <gerardjoven2020@hotmail.com>
2023-11-13 17:41:05 +0100

    Fix route path for pool status.

--- src/routes/api/admin.rs
+++ src/routes/api/admin.rs
@@ -33,7 +33,7 @@ pub fn admin_router(state: UserState) -> Router {
         .nest("/users", user_router)
         .nest("/championships", championships_router)
         .nest("/sockets", socket_router)
-        .route("pools", get(pool_status))
+        .route("/pools", get(pool_status))
         .route_layer(middleware::from_fn(admin_handler))
         .route_layer(middleware::from_fn_with_state(state.clone(), auth_handler))
         .with_state(state)
