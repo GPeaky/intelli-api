f45d5d16ded33502b93b46fba9a59a3e17f89ea9
Gerard Zinn <gerardjoven2020@hotmail.com>
2023-11-13 21:15:50 +0100

    Update User to UserExtension in admin middleware

--- src/middlewares/admin.rs
+++ src/middlewares/admin.rs
@@ -1,5 +1,5 @@
 use crate::{
-    entity::{Role, User},
+    entity::{Role, UserExtension},
     error::{AppResult, UserError},
 };
 use axum::{http::Request, middleware::Next, response::Response};
@@ -8,10 +8,8 @@ use tracing::info;
 pub async fn admin_handler<T>(req: Request<T>, next: Next<T>) -> AppResult<Response> {
     let user = req
         .extensions()
-        .get::<User>()
+        .get::<UserExtension>()
         .ok_or(UserError::Unauthorized)?;
-
-    info!("{:?}", user.role);
 
     if user.role != Role::Admin {
         info!(
