63eafb08621229ae99a6c45d8017addfe3e88789
Gerard Zinn <gerardjoven2020@hotmail.com>
2023-11-13 20:49:21 +0100

    Add debug statement to check user role in admin middleware

--- src/middlewares/admin.rs
+++ src/middlewares/admin.rs
@@ -11,6 +11,8 @@ pub async fn admin_handler<T>(req: Request<T>, next: Next<T>) -> AppResult<Respo
         .get::<User>()
         .ok_or(UserError::Unauthorized)?;
 
+    dbg!(&user.role);
+
     if user.role != Role::Admin {
         info!(
             "User: {}, tried to access to admin area without having admin role",
