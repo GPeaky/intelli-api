0128be2f13c4c3ab2436e2557ab894fd0a61d351
Gerard Zinn <gerardjoven2020@hotmail.com>
2023-12-05 00:44:04 +0100

    Testing buf sizes

--- src/protos/batched.rs
+++ src/protos/batched.rs
@@ -1,6 +1,11 @@
+use std::sync::atomic::{AtomicUsize, Ordering};
+
 use super::{ChunkPacketHeader, PacketHeader};
+use log::info;
 use ntex::util::{Bytes, BytesMut};
 use prost::Message;
+
+static MAX_SIZE: AtomicUsize = AtomicUsize::new(0);
 
 pub struct ToProtoMessageBatched {}
 
@@ -17,6 +22,27 @@ impl ToProtoMessageBatched {
         //TODO: Check if this is enough
         let mut buf = BytesMut::with_capacity(8192);
         data.encode(&mut buf).unwrap();
+
+        {
+            let mut last_value = MAX_SIZE.load(Ordering::Relaxed);
+            let new_value = buf.len();
+
+            while new_value > last_value {
+                let res = MAX_SIZE.compare_exchange_weak(
+                    last_value,
+                    new_value,
+                    Ordering::SeqCst,
+                    Ordering::Relaxed,
+                );
+
+                match res {
+                    Ok(_) => break,
+                    Err(value) => last_value = value,
+                }
+            }
+
+            info!("Max size: {}", MAX_SIZE.load(Ordering::Relaxed));
+        }
 
         Some(buf.freeze())
     }
