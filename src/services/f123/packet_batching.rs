ea8966ecb99826aa618f8453c7cc4ed0aa2efcb6
Gerard Zinn <gerardjoven2020@hotmail.com>
2023-12-07 00:39:01 +0100

    Refactor packet batching implementation

--- src/services/f123/packet_batching.rs
+++ src/services/f123/packet_batching.rs
@@ -33,17 +33,24 @@ impl PacketBatching {
 
         // TODO: Implement another cache method for events
         if let Some(batch) = ToProtoMessageBatched::batched_encoded(self.buf.clone()) {
-            self.cache.set(&batch).await.unwrap();
+            let batch_cloned = batch.clone();
+            let cache_update_fut = self.cache.set(&batch_cloned);
+            let channel_send_fut = self.tx.send(batch);
 
-            if let Err(e) = self.tx.send(batch).await {
-                error!("Error sending batch data: {:?}", e);
+            let (cache_res, channel_res) = tokio::join!(cache_update_fut, channel_send_fut);
+
+            if let Err(e) = cache_res {
+                error!("Error Updating Batching Cache: {:?}", e);
             }
 
-            self.last_batch_time = Instant::now();
+            if let Err(e) = channel_res {
+                error!("Error sending batch data: {:?}", e);
+            }
         } else {
             error!("Error converting and encoding data");
         }
 
+        self.last_batch_time = Instant::now();
         self.buf.clear();
     }
 
@@ -53,22 +60,24 @@ impl PacketBatching {
     pub async fn final_send(&mut self, packet: PacketHeader) {
         self.buf.push(packet);
 
-        if self.buf.is_empty() {
-            return;
-        }
-
         if let Some(batch) = ToProtoMessageBatched::batched_encoded(self.buf.clone()) {
-            self.cache.prune().await.unwrap();
+            let cache_prune_fut = self.cache.prune();
+            let channel_send_fut = self.tx.send(batch);
 
-            if let Err(e) = self.tx.send(batch).await {
-                error!("Error sending batch data: {:?}", e);
+            let (cache_res, channel_res) = tokio::join!(cache_prune_fut, channel_send_fut);
+
+            if let Err(e) = cache_res {
+                error!("Error Pruning Batching Cache: {:?}", e);
             }
 
-            self.last_batch_time = Instant::now();
+            if let Err(e) = channel_res {
+                error!("Error sending batch data: {:?}", e);
+            }
         } else {
             error!("Error converting and encoding data");
         }
 
+        self.last_batch_time = Instant::now();
         self.buf.clear();
     }
 
