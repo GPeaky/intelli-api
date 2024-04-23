d656db3503e051893ce5058c02150d73f09684c2
Gerard Zinn <gerardjoven2020@hotmail.com>
2024-04-23 23:37:36 +0200

    Retuning cached data from stream

--- src/handlers/championships/stream.rs
+++ src/handlers/championships/stream.rs
@@ -3,8 +3,7 @@ use ntex::web::{
     types::{Path, State},
     HttpResponse, Responder,
 };
-use tokio_stream::wrappers::BroadcastStream;
-use tracing::info;
+use tokio_stream::{wrappers::BroadcastStream, StreamExt};
 
 use crate::{
     error::{AppResult, CommonError, F1ServiceError},
@@ -24,17 +23,26 @@ pub async fn handle_stream(
         Err(F1ServiceError::NotActive)?
     }
 
-    let x = state.f1_svc.service_cache(path.0).await?;
-    info!("Cache Size: {}", x.unwrap().len());
+    let cached_data = state.f1_svc.service_cache(path.0).await?;
 
-    // Todo - Get cache from redis and return it before sending real time data
     let Some(rx) = state.f1_svc.subscribe(path.0).await else {
         Err(F1ServiceError::NotActive)?
     };
 
     let stream = BroadcastStream::new(rx);
 
-    Ok(HttpResponse::Ok()
-        .content_type("application/octet-stream")
-        .streaming(stream))
+    match cached_data {
+        None => Ok(HttpResponse::Ok()
+            .content_type("application/octet-stream")
+            .streaming(stream)),
+
+        Some(data) => {
+            let cache_steam = tokio_stream::once(Ok(data));
+            let combined_stream = cache_steam.chain(stream);
+
+            Ok(HttpResponse::Ok()
+                .content_type("application/octet-stream")
+                .streaming(combined_stream))
+        }
+    }
 }
