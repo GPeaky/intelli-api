56cb1b9b3b1fa71cf98d12fc9224ef81f57d3a64
Gerard Zinn <gerardjoven2020@hotmail.com>
2024-09-04 00:48:23 +0200

    .

--- src/services/f1/handler.rs
+++ src/services/f1/handler.rs
@@ -113,9 +113,9 @@ impl F1ServiceHandler {
     }
 
     pub fn unsubscribe(&self, championship_id: &i32) {
-        if let Some(service) = self.services.get(championship_id) {
-            service.unsubscribe();
-        }
+        self.services
+            .get(championship_id)
+            .map(|service| service.unsubscribe());
     }
 
     /// Retrieves a list of all active services.
