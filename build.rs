09f5aedb8b6b58338b75025977b0ad3ae25feff9
Gerard Zinn <gerardjoven2020@hotmail.com>
2024-09-22 19:36:11 +0200

    Bump version

--- build.rs
+++ build.rs
@@ -1,8 +1,6 @@
 fn main() {
     let mut config = prost_build::Config::new();
 
-    // config.bytes(["."]);
-
     config
         .compile_protos(&["protos/f1.proto"], &["protos/"])
         .unwrap();
