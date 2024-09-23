e3e68e3ec55bc49c2c1f9fb077ae59e9078ca447
Gerard Zinn <gerardjoven2020@hotmail.com>
2024-09-24 00:22:04 +0200

    Update conn.rs

--- src/handlers/admin/conn.rs
+++ src/handlers/admin/conn.rs
@@ -1,9 +1,9 @@
 use ntex::web::{types::State, HttpResponse};
 
-use crate::{error::AppResult, states::AppState};
+use crate::states::AppState;
 
 #[inline(always)]
-pub async fn server_active_pools(state: State<AppState>) -> AppResult<HttpResponse> {
+pub async fn server_active_pools(state: State<AppState>) -> HttpResponse {
     let active_pools = state.server_repo.active_pools();
-    Ok(HttpResponse::Ok().json(&active_pools))
+    HttpResponse::Ok().json(&active_pools)
 }
